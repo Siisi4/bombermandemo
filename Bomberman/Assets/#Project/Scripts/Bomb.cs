bd6e5614af6d155816675913391bdd6d05c4fdec
Siisi4 <siisi4officiel@gmail.com>
2020-07-02 16:26:32 +0200

    Lanceur de bombe + bug bombe à corriger

---
+++ Bomberman/Assets/#Project/Scripts/Bomb.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+
+    public float range = 5f;
+    public float timer = 3f;
+    public float force = 500f;
+
+    public GameObject[] walls;
+    public GameObject player;
+    public float firstImpulse = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        walls = GameObject.FindGameObjectsWithTag("Wall");
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * firstImpulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // deltaTime c'est le temps entre 2 frames.
+        // frame en cours et le dernier frame
+        timer -= Time.deltaTime;
+
+        // si le joueur est dans le range de la bombe, il va mourir aussi !
+        if (Vector3.Distance(player.transform.position, transform.position) <= range)
+        {
+            Destroy(gameObject);
+        }
+        if (timer <= 0)
+        {
+            foreach (GameObject wall in walls)
+            {
+                Rigidbody rb = wall.GetComponent<Rigidbody>();
+                rb.AddExplosionForce(force, transform.position, range);
+            }
+            Destroy(gameObject);
+        }
+
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+        Color color = new Color(1f, 0.5f, 0.25f, 0.75f);
+        Gizmos.color = color;
+
+        Gizmos.DrawSphere(transform.position, range);
+    }
+
+}
--- Bomberman/Assets/#Project/Scripts/bomberAI.cs
+++ Bomberman/Assets/#Project/Scripts/bomberAI.cs
@@ -8,23 +8,69 @@ public class bomberAI : MonoBehaviour
 {
     private NavMeshAgent agent;
     public Transform[] targets;
+
+    private GameObject player;
+    public float viewRange = 4f;
+    public float time = 1f;
+    public float actualTimer = 0f;
+
+    private bool playerFound;
+
+    public GameObject bomb;
+  
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         // On commence par lui donner une destination au hasard !
         agent.destination = GiveDestination();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
   
 
     void Update()
     {
+        
+
+        RaycastHit hit;
+
+        if(actualTimer > 0)
+        {
+            actualTimer -= Time.deltaTime;
+        }
+
+        // un vecteur normalisé est réduit à son expression unitaire
+        // donc on a la direction du vecteur mais en (1,1)
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+
+        playerFound = false;
+
+        if (Physics.Raycast(transform.position, direction, out hit, viewRange))
+        {
+            // si j'ai touché le player ?
+            if(hit.transform.gameObject == player)
+            {
+                playerFound = true;
+                // la destination de notre agent est là où est l'objet
+                agent.destination = player.transform.position;
+                if(hit.distance <= 5f)
+                {
+                    if(actualTimer <= 5f)
+                    {
+                        ThrowBomb();
+                        actualTimer = time;
+                    }
+                    
+                }
+            }
+        }
+
+
         // target s'il n'a plus de target //
         if (IsArrived())
         {
-            agent.destination = GiveDestination();
+            agent.destination = GiveDestination();    
         }
-        
     }
 
     // fonction qui donne un ptn au hasard
@@ -40,5 +86,25 @@ public class bomberAI : MonoBehaviour
         // est-ce que la distance entre la target et toi est <= à la distance à parcourir //
         print(agent.remainingDistance - agent.stoppingDistance);
         return agent.remainingDistance <= agent.stoppingDistance;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Color color = Color.green;
+        if (playerFound)
+        {
+            color = Color.red;
+        }
+        color.a = 0.5f;
+        Gizmos.color = color;
+
+        Gizmos.DrawSphere(transform.position, viewRange);
+        Gizmos.DrawWireSphere(transform.position, viewRange);
+    }
+
+    private void ThrowBomb()
+    {
+        GameObject bombInstance = Instantiate(bomb, transform.position, transform.rotation);
+        bombInstance.GetComponent<Bomb>().player = player;
     }
 }
